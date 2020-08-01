966d12cad28da3878cec40090e791c9d3e3a620a
Tlegen Bayangali <tlegmusic@gmail.com>
2020-08-02 04:31:46 +0600

    asd

--- components/partials/portfolio-grid/portfolio-grid.js
+++ components/partials/portfolio-grid/portfolio-grid.js
@@ -3,9 +3,73 @@ export default function PortfolioGrid() {
         <div className="row dalacode-grid">
             <div className="col-lg-6">
                 <div className="portfolio-item">
-                    <img className="portfolio-item__image mb-10" src="/assets/img/guideline-and-brand-book-creation-and-examples-1 1.webp" alt="Брендбук компании Airsoft" />
-
+                    <picture>
+                        <source srcSet={'/assets/img/guideline-and-brand-book-creation-and-examples-1 1.webp'} type="image/webp" />
+                        <img className="portfolio-item__image mb-10"
+                             src="/assets/img/guideline-and-brand-book-creation-and-examples-1 1.jpg"
+                             alt="Брендбук компании Airsoft" />
+                    </picture>
                     <h6 className="mb-10">
+                        Брендбук компании Airsoft
+                    </h6>
+                    <a href="#" className="read-more">
+                        Подробнее
+                        <svg width="12" height="6" viewBox="0 0 12 6" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.0075 2.25H0V3.75H9.0075V6L12 3L9.0075 0V2.25Z" fill="#304163"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div className="col-lg-6">
+                <div className="portfolio-item">
+                    <picture>
+                        <source srcSet={'/assets/img/guideline-and-brand-book-creation-and-examples-guid 1.webp'} type="image/webp" />
+                        <img className="portfolio-item__image mb-10"
+                             src="/assets/img/guideline-and-brand-book-creation-and-examples-guid 1.jpg"
+                             alt="Брендбук компании Applesin" />
+                    </picture>
+                        <h6 className="mb-10">
+                            Брендбук компании Applesin
+                        </h6>
+                        <a href="#" className="read-more">
+                            Подробнее
+                            <svg width="12" height="6" viewBox="0 0 12 6" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9.0075 2.25H0V3.75H9.0075V6L12 3L9.0075 0V2.25Z" fill="#304163"/>
+                            </svg>
+                        </a>
+                </div>
+            </div>
+            <div className="col-lg-6">
+                <div className="portfolio-item">
+                    <picture>
+                        <source srcSet={'/assets/img/guideline-and-brand-book-creation-and-examples-guid 1.webp'} type="image/webp" />
+                        <img className="portfolio-item__image mb-10"
+                             src="/assets/img/guideline-and-brand-book-creation-and-examples-guid 1.jpg"
+                             alt="Брендбук компании Applesin" />
+                    </picture>
+                        <h6 className="mb-10">
+                            Брендбук компании Applesin
+                        </h6>
+                        <a href="#" className="read-more">
+                            Подробнее
+                            <svg width="12" height="6" viewBox="0 0 12 6" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9.0075 2.25H0V3.75H9.0075V6L12 3L9.0075 0V2.25Z" fill="#304163"/>
+                            </svg>
+                        </a>
+                </div>
+            </div>
+            <div className="col-lg-6">
+                <div className="portfolio-item">
+                    <picture>
+                        <source className="portfolio-item__image mb-10" srcSet={'/assets/img/guideline-and-brand-book-creation-and-examples-1 1.webp'} type="image/webp" />
+                        <img className="portfolio-item__image mb-10"
+                             src="/assets/img/guideline-and-brand-book-creation-and-examples-1 1.jpg"
+                             alt="Брендбук компании Airsoft" />
+                    </picture>
+                        <h6 className="mb-10">
                             Брендбук компании Airsoft
                         </h6>
                         <a href="#" className="read-more">
@@ -17,69 +81,6 @@ export default function PortfolioGrid() {
                         </a>
                 </div>
             </div>
-            {/*<div className="col-lg-6">*/}
-            {/*    <div className="portfolio-item">*/}
-            {/*        <picture>*/}
-            {/*            <source srcSet={'/assets/img/guideline-and-brand-book-creation-and-examples-guid 1.webp'} type="image/webp" />*/}
-            {/*            <source srcSet={'/assets/img/guideline-and-brand-book-creation-and-examples-guid 1.jpg'} type="image/jpeg" />*/}
-            {/*            <img className="portfolio-item__image mb-10"*/}
-            {/*                 src="/assets/img/guideline-and-brand-book-creation-and-examples-guid 1.jpg"*/}
-            {/*                 alt="Брендбук компании Applesin" />*/}
-            {/*        </picture>*/}
-            {/*            <h6 className="mb-10">*/}
-            {/*                Брендбук компании Applesin*/}
-            {/*            </h6>*/}
-            {/*            <a href="#" className="read-more">*/}
-            {/*                Подробнее*/}
-            {/*                <svg width="12" height="6" viewBox="0 0 12 6" fill="none"*/}
-            {/*                     xmlns="http://www.w3.org/2000/svg">*/}
-            {/*                    <path d="M9.0075 2.25H0V3.75H9.0075V6L12 3L9.0075 0V2.25Z" fill="#304163"/>*/}
-            {/*                </svg>*/}
-            {/*            </a>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-            {/*<div className="col-lg-6">*/}
-            {/*    <div className="portfolio-item">*/}
-            {/*        <picture>*/}
-            {/*            <source srcSet={'/assets/img/guideline-and-brand-book-creation-and-examples-guid 1.webp'} type="image/webp" />*/}
-            {/*            <source srcSet={'/assets/img/guideline-and-brand-book-creation-and-examples-guid 1.jpg'} type="image/jpeg" />*/}
-            {/*            <img className="portfolio-item__image mb-10"*/}
-            {/*                 src="/assets/img/guideline-and-brand-book-creation-and-examples-guid 1.jpg"*/}
-            {/*                 alt="Брендбук компании Applesin" />*/}
-            {/*        </picture>*/}
-            {/*            <h6 className="mb-10">*/}
-            {/*                Брендбук компании Applesin*/}
-            {/*            </h6>*/}
-            {/*            <a href="#" className="read-more">*/}
-            {/*                Подробнее*/}
-            {/*                <svg width="12" height="6" viewBox="0 0 12 6" fill="none"*/}
-            {/*                     xmlns="http://www.w3.org/2000/svg">*/}
-            {/*                    <path d="M9.0075 2.25H0V3.75H9.0075V6L12 3L9.0075 0V2.25Z" fill="#304163"/>*/}
-            {/*                </svg>*/}
-            {/*            </a>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-            {/*<div className="col-lg-6">*/}
-            {/*    <div className="portfolio-item">*/}
-            {/*        <picture>*/}
-            {/*            <source srcSet={'/assets/img/guideline-and-brand-book-creation-and-examples-1 1.webp'} type="image/webp" />*/}
-            {/*            <source srcSet={'/assets/img/guideline-and-brand-book-creation-and-examples-1 1.jpg'} type="image/jpeg" />*/}
-            {/*            <img className="portfolio-item__image mb-10"*/}
-            {/*                 src="/assets/img/guideline-and-brand-book-creation-and-examples-1 1.jpg"*/}
-            {/*                 alt="Брендбук компании Airsoft" />*/}
-            {/*        </picture>*/}
-            {/*            <h6 className="mb-10">*/}
-            {/*                Брендбук компании Airsoft*/}
-            {/*            </h6>*/}
-            {/*            <a href="#" className="read-more">*/}
-            {/*                Подробнее*/}
-            {/*                <svg width="12" height="6" viewBox="0 0 12 6" fill="none"*/}
-            {/*                     xmlns="http://www.w3.org/2000/svg">*/}
-            {/*                    <path d="M9.0075 2.25H0V3.75H9.0075V6L12 3L9.0075 0V2.25Z" fill="#304163"/>*/}
-            {/*                </svg>*/}
-            {/*            </a>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </div>
     )
 }
